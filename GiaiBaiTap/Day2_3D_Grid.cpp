dd4a8428acebf8bdbf3e91d5688a0294c90ffe4b
toanlinka <toanlinka@outlook.com>
2022-10-02 11:36:39 +0700

    upload_9

---
+++ GiaiBaiTap/Day2_3D_Grid.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+struct{
+	int x;
+	int y;
+	int z;
+}myStruct;
+
+char a[31][31][31];
+queue<myStruct> q;
+
+int dx[]={1};
+int dy[]={1};
+int dz[]={1};
+
+void BFS(int x, int y,int z){
+	
+}
+int main(){
+	return 0;
+}
