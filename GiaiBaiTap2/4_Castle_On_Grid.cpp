9a383aa0ff076bb4060ffbc5d877e837810ade5e
toanlinka <toanlinka@outlook.com>
2022-10-13 13:52:17 +0700

    HuyBlue9

--- GiaiBaiTap2/4_Castle_On_Grid.cpp
+++ GiaiBaiTap2/4_Castle_On_Grid.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 int n;
+int b_x,b_y,g_x,g_y;
 char a[101][101];
 int Free[101][101];
 int Step[101][101];
@@ -13,25 +14,31 @@ int dx[]={0,-1,1,0};
 int dy[]={-1,0,0,1};
 queue <pair<int,int> > q;
 
-int BFS(int x,int y){
+int BFS(int x,int y,int g_x, int g_y){
 	q.push({x,y});
 	Free[x][y]=1;
 	while(!q.empty()){
 		pair<int,int> top = q.front();
 		q.pop();
 		for(int k=0;k<4;k++){
-			while()
-			int i1 = top.first + dx[k];
-			int j1 = top.second + dy[k];
-			
-			if(i1>=0 && i1<n && j1>=0 && j1<n && a[i1][j1]!='X' && Free[i1][j1]==0){
-				Free[i1][j1]=1;
-				q.push({i1,j1});
-				Step[i1][j1] = Step[top.first][top.second] + 1;
-				if(i1==g_x && j1 ==g_y){
-					return Step[i1][j1];
+			int count = 1;
+			while(count<=n){
+				int i1 = top.first + dx[k]*count;
+				int j1 = top.second + dy[k]*count;
+				count++;
+				if(a[i1][j1]=='X'||i1<0||i1>=n||j1<0||j1>=n){
+					break;
 				}
+				if(i1>=0 && i1<n && j1>=0 && j1<n && a[i1][j1]!='X' && Free[i1][j1]==0){
+					Free[i1][j1]=1;
+					q.push({i1,j1});
+					Step[i1][j1] = Step[top.first][top.second] + 1;
+					if(i1==g_x && j1 ==g_y){
+						return Step[i1][j1];
+					}
+				}					
 			}
+
 		}
 	}
 	return -1;
@@ -44,5 +51,7 @@ int main(){
 			cin>>a[i][j];
 		}
 	}
+	cin>>b_x>>b_y>>g_x>>g_y;
+	cout<<BFS(b_x,b_y,g_x,g_y);
 	return 0;
 }
